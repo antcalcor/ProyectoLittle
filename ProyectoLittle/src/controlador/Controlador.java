6f7697e57e9e55d95668a839c4d68f341bd7ec86
Usuario <Usuario@192.168.31.162>
2018-09-27 10:39:50 +0200

    empezando con el controlador no he terminado

--- ProyectoLittle/src/controlador/Controlador.java
+++ ProyectoLittle/src/controlador/Controlador.java
@@ -5,10 +5,82 @@
  */
 package controlador;
 
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import vista.PantallaInicial;
+
 /**
  *
  * @author Usuario
  */
-public class Controlador {
+public class Controlador implements ActionListener, MouseListener {
+
+    public PantallaInicial pIni = new PantallaInicial();
+    
+    //Declaramos enum de acciones
+    public enum accionesPIni {
+        
+        CLIENTE, ARTICULO, FACTURA;
+        
+    }
+    
+    //Constructor
+    public Controlador (PantallaInicial pIni){
+        
+        this.pIni=pIni;
+        
+    }
+    
+    //Iniciamos
+    public void Iniciar(){
+        try{
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			SwingUtilities.updateComponentTreeUI(pIni);
+			pIni.setVisible(true);
+			pIni.setLocationRelativeTo(null);
+                        pIni.setExtendedState(MAXIMIZED_BOTH);
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+        }
+    }
+    
+    //Declaramos las acciones y anidamos las escuchas al evento producido por el componente
+    //tengo que seguir por aqui mirar controlador acceso proyecto final
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
